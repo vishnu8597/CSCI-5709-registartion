40eac0f856ebee1b0ce2bfee28b00dd690320a7a
Vishnu Sumanth Dampetla <vs683406@dal.ca>
2022-03-09 12:20:39 -0400

    added state and cognito creds

---
+++ src/cognito/Userpool.js
@@ -0,0 +1,7 @@
+import {UserPool} from "amazon-cognito-identity-js"
+const pooldarams={
+    UserPoolId:"us-east-1_QqnQp5cZX",
+    ClientId:"6t1kup4jkjs65mf84u3ttuauot"
+}
+
+export default new UserPool(pooldarams)
--- src/componenets/Form.jsx
+++ src/componenets/Form.jsx
@@ -1,32 +1,112 @@
+import React, { useState } from "react";
+
 function Form() {
+  const onsubmit = (event) => {
+    event.preventDefault();
+  };
+  const [formData, updateFormData] = useState({
+    fname: "",
+    lname: "",
+    age: "",
+    Address: "",
+    gender: "1",
+    email: "",
+    phone: "",
+    password: "",
+    blood: "O+",
+    td: "0",
+    yon: "",
+  });
+  function updateFname(event) {
+    updateFormData({ ...formData, fname: event.target.value });
+  }
+  function updateLname(event) {
+    updateFormData({ ...formData, lname: event.target.value });
+  }
+  function updateAge(event) {
+    updateFormData({ ...formData, age: event.target.value });
+  }
+  function updateAddress(event) {
+    updateFormData({ ...formData, Address: event.target.value });
+  }
+  function updateGender(event) {
+    updateFormData({ ...formData, gender: event.target.value });
+  }
+  function updateEmail(event) {
+    updateFormData({ ...formData, email: event.target.value });
+  }
+  function updatephone(event) {
+    updateFormData({ ...formData, phone: event.target.value });
+  }
+  function updatePassword(event) {
+    updateFormData({ ...formData, password: event.target.value });
+  }
+  function updateBlood(event) {
+    updateFormData({ ...formData, blood: event.target.value });
+  }
+  function updateTd(event) {
+    updateFormData({ ...formData, td: event.target.value });
+  }
+  function updateYon(event) {
+    updateFormData({ ...formData, yon: event.target.value });
+  }
+
   return (
     <div>
       <h2>BE a helping hand</h2>
       <h5>Welcome to canada's biggest online blood bank system</h5>
-      <form post="/home">
+      <form onSubmit={onsubmit} post="/home">
         <p>
           <label>First Name</label>
           <br />
-          <input type="text" name="first_name" required />
+          <input
+            type="text"
+            name="first_name"
+            onChange={updateFname}
+            value={formData.fname}
+            required
+          />
         </p>
         <p>
           <label>Last Name</label>
           <br />
-          <input type="text" name="last_name" required />
+          <input
+            type="text"
+            name="last_name"
+            onChange={updateLname}
+            value={formData.lname}
+            required
+          />
         </p>
         <p>
           <label>Age</label>
           <br />
-          <input type="text" name="Age" required />
+          <input
+            type="text"
+            name="Age"
+            onChange={updateAge}
+            value={formData.age}
+            required
+          />
         </p>
         <p>
           <label>Address</label>
           <br />
-          <textarea name="Address" required />
+          <textarea
+            name="Address"
+            onChange={updateAddress}
+            value={formData.Address}
+            required
+          />
         </p>
         <label for="gender">Gender:</label>
 
-        <select name="gender" id="mf">
+        <select
+          name="gender"
+          onChange={updateGender}
+          value={formData.gender}
+          id="mf"
+        >
           <option value="1">Male</option>
           <option value="0">Female</option>
         </select>
@@ -34,21 +114,44 @@ function Form() {
         <p>
           <label>Email address</label>
           <br />
-          <input type="email" name="email" required />
+          <input
+            type="email"
+            name="email"
+            onChange={updateEmail}
+            value={formData.email}
+            required
+          />
         </p>
         <p>
           <label>Phone</label>
           <br />
-          <input type="text" name="phone" required />
+          <input
+            type="text"
+            name="phone"
+            value={formData.phone}
+            onChange={updatephone}
+            required
+          />
         </p>
         <p>
           <label>Password</label>
           <br />
-          <input type="password" name="password" requiredc />
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={updatePassword}
+            required
+          />
         </p>
         <label for="blood">Choose your blood type:</label>
 
-        <select name="Blood_type" id="bt">
+        <select
+          name="Blood_type"
+          onChange={updateBlood}
+          value={formData.blood}
+          id="bt"
+        >
           <option value="O+">O+</option>
           <option value="O-">O-</option>
           <option value="A+">A+</option>
@@ -62,13 +165,24 @@ function Form() {
         <p>
           <label>Number of time you have doanted blood</label>
           <br />
-          <input type="text" name="nbd" required />
+          <input
+            type="text"
+            name="nbd"
+            onChange={updateTd}
+            value={formData.td}
+            required
+          />
         </p>
         <label for="blood">
           If you were asked to donate blood now. will you donate ?:
         </label>
 
-        <select name="yesorno" id="yon">
+        <select
+          name="yesorno"
+          id="yon"
+          onChange={updateYon}
+          value={formData.yon}
+        >
           <option value="1">yes</option>
           <option value="0">no</option>
         </select>
