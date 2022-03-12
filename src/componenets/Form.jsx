47b616e78aa85094781e022661a1aa5a07da852e
VENKATA SAIKIRAN KATTEKOLA <vn857734@dal.ca>
2022-03-11 23:39:33 -0400

    Integrated bootstrap in the application

--- src/componenets/Form.jsx
+++ src/componenets/Form.jsx
@@ -80,158 +80,187 @@ function Form() {
   return (
     <div>
       <h2>Be a helping hand</h2>
-      <h5>Welcome to canada's biggest online blood bank system</h5>
+      <h5 style={{ "border-bottom": "5px solid #ddd" }}>
+        Welcome to Canada's biggest online blood bank system
+      </h5>
+      <h2 style={{ "margin-left": "30px" }}>Register as - Donor</h2>
       <form onSubmit={onsubmit} post="/home">
-        <p>
-          <label>First Name</label>
-          <br />
-          <input
-            type="text"
-            name="first_name"
-            onChange={updateFname}
-            value={formData.fname}
-            required
-          />
-        </p>
-        <p>
-          <label>Last Name</label>
-          <br />
-          <input
-            type="text"
-            name="last_name"
-            onChange={updateLname}
-            value={formData.lname}
-            required
-          />
-        </p>
-        <p>
-          <label>Age</label>
-          <br />
-          <input
-            type="text"
-            name="Age"
-            onChange={updateAge}
-            value={formData.age}
-            required
-          />
-        </p>
-        <p>
-          <label>Address</label>
-          <br />
-          <textarea
-            name="Address"
-            onChange={updateAddress}
-            value={formData.Address}
-            required
-          />
-        </p>
-        <label for="gender">Gender:</label>
+        <div class="form-group row">
+          <label class="col-sm-4 col-form-label">First Name</label>
+          <div class="col-sm-4">
+            <input
+              type="text"
+              class="form-control"
+              name="first_name"
+              onChange={updateFname}
+              value={formData.fname}
+              required
+            />
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-4 col-form-label">Last Name</label>
+          <div class="col-sm-4">
+            <input
+              type="text"
+              class="form-control"
+              name="last_name"
+              onChange={updateLname}
+              value={formData.lname}
+              required
+            />
+          </div>
+        </div>
 
-        <select
-          name="gender"
-          onChange={updateGender}
-          value={formData.gender}
-          id="mf"
-        >
-          <option value="1">Male</option>
-          <option value="0">Female</option>
-        </select>
-
-        <p>
-          <label>Email address</label>
-          <br />
-          <input
-            type="email"
-            name="email"
-            onChange={updateEmail}
-            value={formData.email}
-            required
-          />
-        </p>
-        <p>
-          <label>Phone</label>
-          <br />
-          <input
-            type="text"
-            name="phone"
-            value={formData.phone}
-            onChange={updatephone}
-            required
-          />
-        </p>
-        <p>
-          <label>Password</label>
-          <br />
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={updatePassword}
-            required
-          />
-        </p>
-        <label for="blood">Choose your blood type:</label>
-
-        <select
-          name="Blood_type"
-          onChange={updateBlood}
-          value={formData.blood}
-          id="bt"
-        >
-          <option value="O+">O+</option>
-          <option value="O-">O-</option>
-          <option value="A+">A+</option>
-          <option value="A-">A-</option>
-          <option value="B+">B+</option>
-          <option value="B-">B-</option>
-          <option value="AB+">AB+</option>
-          <option value="AB-">AB-</option>
-        </select>
-
-        <p>
-          <label>Number of time you have doanted blood</label>
-          <br />
-          <input
-            type="text"
-            name="nbd"
-            onChange={updateTd}
-            value={formData.td}
-            required
-          />
-        </p>
-        <label for="blood">
-          If you were asked to donate blood now. will you donate ?:
-        </label>
-
-        <select
-          name="yesorno"
-          id="yon"
-          onChange={updateYon}
-          value={formData.yon}
-        >
-          <option value="1">yes</option>
-          <option value="0">no</option>
-        </select>
-
-        <p>
-          <input type="checkbox" name="checkbox" id="checkbox" required />{" "}
-          <span>
-            I agree all statements in{" "}
-            <a
-              href="https://google.com"
-              target="_blank"
-              rel="noopener noreferrer"
+        <div class="form-group row">
+          <label class="col-sm-4 col-form-label">Age</label>
+          <div class="col-sm-4">
+            <input
+              type="text"
+              class="form-control"
+              name="Age"
+              onChange={updateAge}
+              value={formData.age}
+              required
+            />
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-4 col-form-label">Address</label>
+          <div class="col-sm-4">
+            <textarea
+              name="Address"
+              class="form-control"
+              onChange={updateAddress}
+              value={formData.Address}
+              required
+            />
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-4 col-form-label">Gender</label>
+          <div class="col-sm-4">
+            <select
+              class="form-select"
+              name="gender"
+              onChange={updateGender}
+              value={formData.gender}
+              id="mf"
             >
-              terms of service
-            </a>
-          </span>
-          .
-        </p>
-        <p>
-          <button id="sub_btn" type="submit">
-            Register
-          </button>
-        </p>
+              <option value="1">Male</option>
+              <option value="0">Female</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-4 col-form-label">Email address</label>
+          <div class="col-sm-4">
+            <input
+              type="email"
+              class="form-control"
+              name="email"
+              onChange={updateEmail}
+              value={formData.email}
+              required
+            />
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-4 col-form-label">Phone</label>
+          <div class="col-sm-4">
+            <input
+              type="text"
+              name="phone"
+              class="form-control"
+              value={formData.phone}
+              onChange={updatephone}
+              required
+            />
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-4 col-form-label">Password</label>
+          <div class="col-sm-4">
+            <input
+              type="password"
+              class="form-control"
+              name="password"
+              value={formData.password}
+              onChange={updatePassword}
+              required
+            />
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-4 col-form-label">Choose your blood type</label>
+          <div class="col-sm-4">
+            <select
+              name="Blood_type"
+              class="form-select"
+              onChange={updateBlood}
+              value={formData.blood}
+              id="bt"
+            >
+              <option value="O+">O+</option>
+              <option value="O-">O-</option>
+              <option value="A+">A+</option>
+              <option value="A-">A-</option>
+              <option value="B+">B+</option>
+              <option value="B-">B-</option>
+              <option value="AB+">AB+</option>
+              <option value="AB-">AB-</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-4 col-form-label">
+            Number of times you have donated blood
+          </label>
+          <div class="col-sm-4">
+            <input
+              type="text"
+              class="form-control"
+              name="nbd"
+              onChange={updateTd}
+              value={formData.td}
+              required
+            />
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-4 col-form-label" for="blood">
+            If you were asked to donate blood now. will you donate ?
+          </label>
+          <div class="col-sm-4">
+            <select
+              name="yesorno"
+              id="yon"
+              class="form-select"
+              onChange={updateYon}
+              value={formData.yon}
+            >
+              <option value="1">yes</option>
+              <option value="0">no</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-check">
+          <input
+            type="checkbox"
+            class="form-check-input"
+            name="checkbox"
+            id="checkbox"
+            style={{ float: "inherit" }}
+            required
+          />{" "}
+          <label class="form-check-label">
+            I agree all statements in terms of service .
+          </label>
+        </div>
+
+        <button id="sub_btn" type="submit" class="btn btn-primary">
+          Register
+        </button>
       </form>
       {/* <footer>
         <p>
